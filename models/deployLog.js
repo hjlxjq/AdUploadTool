9ef1543576efb7eb899672aa0ffaba9f18982d29
Jianlong Hu <jianlong@talefun.com>
2020-08-11 20:34:48 +0800

    发布日志

---
+++ models/deployLog.js
@@ -0,0 +1,22 @@
+const db = require('../tools/db');
+
+module.exports = db.defineModel('deployLog', {
+    log: {
+        type: db.STRING(500),
+        allowNull: false,
+    },
+    type: {
+        type: db.BOOLEAN,
+        allowNull: false,
+    },
+    productId: {
+        type: db.CHAR(36),
+        allowNull: false,
+    },
+    userId: {
+        type: db.CHAR(36),
+        allowNull: false,
+        unique: true
+    }
+
+});
